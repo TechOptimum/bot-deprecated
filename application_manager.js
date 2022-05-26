97c3594b8a3e35f90c85d369ed2ebc111448d72a
SiddharthD <96356209+siddharthd0@users.noreply.github.com>
2022-05-26 23:30:38 +0000

    Formatting

--- application_manager.js
+++ application_manager.js
@@ -1,10 +1,10 @@
 const {
-  Client,
-  MessageActionRow,
-  MessageButton,
-  MessageEmbed,
-  Modal,
-  TextInputComponent,
+    Client,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
+    Modal,
+    TextInputComponent,
 } = require("discord.js");
 const settings = require("./config");
 
@@ -14,224 +14,224 @@ const settings = require("./config");
  * @param {settings} settings
  */
 module.exports = async (client, settings) => {
-  // code
+    // code
 
-  client.on("interactionCreate", async (interaction) => {
-    if (interaction.isCommand()) {
-      switch (interaction.commandName) {
-        case "setup":
-          {
-            let applyChannel = interaction.guild.channels.cache.get(
-              settings.applyChannel
+    client.on("interactionCreate", async (interaction) => {
+        if (interaction.isCommand()) {
+            switch (interaction.commandName) {
+                case "setup":
+                    {
+                        let applyChannel = interaction.guild.channels.cache.get(
+                            settings.applyChannel
+                        );
+                        if (!applyChannel) return;
+
+                        let btnrow = new MessageActionRow().addComponents([
+                            /* new MessageButton()
+                               .setStyle("PRIMARY")
+                               .setCustomId("ap_ping")
+                               .setLabel("Ping me !!")
+                               .setEmoji("📶"),*/
+                            new MessageButton()
+                                .setStyle("SUCCESS")
+                                .setCustomId("ap_apply")
+                                .setLabel("Apply")
+                                .setEmoji("📑"),
+                        ]);
+                        applyChannel.send({
+                            embeds: [
+                                new MessageEmbed()
+                                    .setColor("BLURPLE")
+                                    .setTitle(`${interaction.guild.name}`)
+                                    .setDescription(
+                                        `> Please click on the apply button to begin the application.`)
+                                    .setFooter(`Developer Applications`)
+                                ,
+                            ],
+                            components: [btnrow],
+                        });
+
+                        interaction.reply({
+                            content: `> Setup in ${applyChannel}`,
+                        });
+                    }
+                    break;
+                case "ping":
+                    {
+                        interaction.reply({
+                            content: `pong :: ${client.ws.ping}`,
+                            ephemeral: true,
+                        });
+                    }
+                    break;
+
+                default:
+                    interaction.reply({
+                        content: `command not found ${interaction.commandName}`,
+                        ephemeral: true,
+                    });
+                    break;
+            }
+        }
+
+        // for buttons
+        if (interaction.isButton()) {
+            switch (interaction.customId) {
+                case "ap_ping":
+                    {
+                        interaction.reply({
+                            content: `i am working , now you can apply`,
+                            ephemeral: true,
+                        });
+                    }
+                    break;
+
+                case "ap_apply":
+                    {
+                        let application_modal = new Modal()
+                            .setTitle(`Application System`)
+                            .setCustomId(`application_modal`);
+
+                        const user_name = new TextInputComponent()
+                            .setCustomId("ap_username")
+                            .setLabel(`What is your real name?`.substring(0, 45))
+                            .setMinLength(4)
+                            .setMaxLength(50)
+                            .setRequired(true)
+                            .setPlaceholder(`Please type your first and last name`)
+                            .setStyle("SHORT");
+
+                        const user_why = new TextInputComponent()
+                            .setCustomId("ap_userwhy")
+                            .setLabel(`Why you are applying for staff?`.substring(0, 45))
+                            .setMinLength(4)
+                            .setMaxLength(100)
+                            .setRequired(true)
+                            .setPlaceholder(`Share with us why you would like to join our team`)
+                            .setStyle("PARAGRAPH");
+
+                        const user_experience = new TextInputComponent()
+                            .setCustomId("ap_user_experience")
+                            .setLabel(`What previous experience do you carry?`.substring(0, 45))
+                            .setMinLength(4)
+                            .setMaxLength(100)
+                            .setRequired(true)
+                            .setPlaceholder(`Share your work experience/volunteer experience`)
+                            .setStyle("PARAGRAPH");
+
+                        let row_username = new MessageActionRow().addComponents(user_name);
+                        let row_userwhy = new MessageActionRow().addComponents(user_why);
+                        let row_userexperience = new MessageActionRow().addComponents(user_experience);
+
+                        application_modal.addComponents(row_username, row_userwhy, row_userexperience);
+
+                        await interaction.showModal(application_modal);
+                    }
+                    break;
+                case "ap_accept":
+                    {
+                        let embed = new MessageEmbed(
+                            interaction.message.embeds[0]
+                        ).setColor("GREEN");
+
+                        interaction.message.edit({
+                            embeds: [embed],
+                            components: [],
+                        });
+
+                        let ap_user = interaction.guild.members.cache.get(
+                            embed.footer.text
+                        );
+
+                        ap_user.send(`**Congratulations  **  🎉\n The application you submitted to become a Developer was accepted!\n Please join our Staff server: https://discord.gg/cVGv2Ttx5F\n The executive management team will soon give you your roles upon joining. `).catch(e => { })
+
+                        await interaction.member.roles.add(settings.helperrole).catch(e => { })
+                        await interaction.member.roles.remove(settings.waitingrole).catch(e => { })
+                    }
+                    break;
+                case "ap_reject":
+                    {
+                        let embed = new MessageEmbed(
+                            interaction.message.embeds[0]
+                        ).setColor("RED");
+
+                        interaction.message.edit({
+                            embeds: [embed],
+                            components: [],
+                        });
+
+                        let ap_user = interaction.guild.members.cache.get(
+                            embed.footer.text
+                        );
+
+                        ap_user.send(`We're sorry to say this... But, your application for Developer was denied.`).catch(e => { })
+                        await interaction.member.roles.remove(settings.waitingrole).catch(e => { })
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // for modals
+        if (interaction.isModalSubmit()) {
+            let user_name = interaction.fields.getTextInputValue("ap_username");
+            let user_why = interaction.fields.getTextInputValue("ap_userwhy");
+            let user_experience = interaction.fields.getTextInputValue("ap_user_experience");
+
+            let finishChannel = interaction.guild.channels.cache.get(
+                settings.finishChannel
             );
-            if (!applyChannel) return;
-
+            if (!finishChannel) return;
             let btnrow = new MessageActionRow().addComponents([
-             /* new MessageButton()
-                .setStyle("PRIMARY")
-                .setCustomId("ap_ping")
-                .setLabel("Ping me !!")
-                .setEmoji("📶"),*/
-              new MessageButton()
-                .setStyle("SUCCESS")
-                .setCustomId("ap_apply")
-                .setLabel("Apply")
-                .setEmoji("📑"),
+                new MessageButton()
+                    .setStyle("SECONDARY")
+                    .setCustomId("ap_accept")
+                    .setLabel("Accept")
+                    .setEmoji("✅"),
+                new MessageButton()
+                    .setStyle("SECONDARY")
+                    .setCustomId("ap_reject")
+                    .setLabel("Reject")
+                    .setEmoji("❌"),
             ]);
-            applyChannel.send({
-              embeds: [
-                new MessageEmbed()
-                  .setColor("BLURPLE")
-                  .setTitle(`${interaction.guild.name}`)
-                  .setDescription(
-                    `> Please click on the apply button to begin the application.`)
-                   .setFooter(`Developer Applications`)
-                  ,
-              ],
-              components: [btnrow],
+
+            finishChannel.send({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor("BLURPLE")
+                        .setTitle(`Application From ${interaction.user.tag}`)
+                        .setDescription(
+                            `${interaction.user} <t:${Math.floor(Date.now() / 1000)}:R>`
+                        )
+                        .addFields([
+                            {
+                                name: `What is your name?`,
+                                value: `> ${user_name}`,
+                            },
+                            {
+                                name: `Why do you want to join staff?`,
+                                value: `> ${user_why}`,
+                            },
+                            {
+                                name: `What previous experience do you carry?`,
+                                value: `> ${user_experience}`,
+                            },
+                        ])
+                        .setFooter({
+                            text: `${interaction.user.id}`,
+                            iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+                        }),
+                ],
+                components: [btnrow],
             });
 
             interaction.reply({
-              content: `> Setup in ${applyChannel}`,
-            });
-          }
-          break;
-        case "ping":
-          {
-            interaction.reply({
-              content: `pong :: ${client.ws.ping}`,
-              ephemeral: true,
-            });
-          }
-          break;
-
-        default:
-          interaction.reply({
-            content: `command not found ${interaction.commandName}`,
-            ephemeral: true,
-          });
-          break;
-      }
-    }
-
-    // for buttons
-    if (interaction.isButton()) {
-      switch (interaction.customId) {
-        case "ap_ping":
-          {
-            interaction.reply({
-              content: `i am working , now you can apply`,
-              ephemeral: true,
-            });
-          }
-          break;
-
-        case "ap_apply":
-          {
-            let application_modal = new Modal()
-              .setTitle(`Application System`)
-              .setCustomId(`application_modal`);
-
-            const user_name = new TextInputComponent()
-              .setCustomId("ap_username")
-              .setLabel(`What is your real name?`.substring(0, 45))
-              .setMinLength(4)
-              .setMaxLength(50)
-              .setRequired(true)
-              .setPlaceholder(`Please type your first and last name`)
-              .setStyle("SHORT");
-
-            const user_why = new TextInputComponent()
-              .setCustomId("ap_userwhy")
-              .setLabel(`Why you are applying for staff?`.substring(0, 45))
-              .setMinLength(4)
-              .setMaxLength(100)
-              .setRequired(true)
-              .setPlaceholder(`Share with us why you would like to join our team`)
-              .setStyle("PARAGRAPH");
-              
-            const user_experience = new TextInputComponent()
-              .setCustomId("ap_user_experience")
-              .setLabel(`What previous experience do you carry?`.substring(0, 45))
-              .setMinLength(4)
-              .setMaxLength(100)
-              .setRequired(true)
-              .setPlaceholder(`Share your work experience/volunteer experience`)
-              .setStyle("PARAGRAPH");
-
-            let row_username = new MessageActionRow().addComponents(user_name);
-            let row_userwhy = new MessageActionRow().addComponents(user_why);
-             let row_userexperience = new MessageActionRow().addComponents(user_experience);
-              
-            application_modal.addComponents(row_username, row_userwhy, row_userexperience);
-
-            await interaction.showModal(application_modal);
-          }
-          break;
-        case "ap_accept":
-          {
-            let embed = new MessageEmbed(
-              interaction.message.embeds[0]
-            ).setColor("GREEN");
-
-            interaction.message.edit({
-              embeds: [embed],
-              components: [],
+                content: `Good job! You're application has been submitted for review. Please give us a few days/weeks to review your application.`,
+                ephemeral: true,
             });
 
-            let ap_user = interaction.guild.members.cache.get(
-              embed.footer.text
-            );
-
-            ap_user.send(`**Congratulations  **  🎉\n The application you submitted to become a Developer was accepted!\n Please join our Staff server: https://discord.gg/cVGv2Ttx5F\n The executive management team will soon give you your roles upon joining. `).catch(e => {})
-
-            await interaction.member.roles.add(settings.helperrole).catch(e => {})
-            await interaction.member.roles.remove(settings.waitingrole).catch(e => {})
-          }
-          break;
-        case "ap_reject":
-          {
-            let embed = new MessageEmbed(
-              interaction.message.embeds[0]
-            ).setColor("RED");
-
-            interaction.message.edit({
-              embeds: [embed],
-              components: [],
-            });
-
-            let ap_user = interaction.guild.members.cache.get(
-              embed.footer.text
-            );
-
-            ap_user.send(`We're sorry to say this... But, your application for Developer was denied.`).catch(e => {})
-            await interaction.member.roles.remove(settings.waitingrole).catch(e => {})
-          }
-          break;
-        default:
-          break;
-      }
-    }
-
-    // for modals
-    if (interaction.isModalSubmit()) {
-      let user_name = interaction.fields.getTextInputValue("ap_username");
-      let user_why = interaction.fields.getTextInputValue("ap_userwhy");
-     let user_experience = interaction.fields.getTextInputValue("ap_user_experience");
-   
-      let finishChannel = interaction.guild.channels.cache.get(
-        settings.finishChannel
-      );
-      if (!finishChannel) return;
-      let btnrow = new MessageActionRow().addComponents([
-        new MessageButton()
-          .setStyle("SECONDARY")
-          .setCustomId("ap_accept")
-          .setLabel("Accept")
-          .setEmoji("✅"),
-        new MessageButton()
-          .setStyle("SECONDARY")
-          .setCustomId("ap_reject")
-          .setLabel("Reject")
-          .setEmoji("❌"),
-      ]);
-
-      finishChannel.send({
-        embeds: [
-          new MessageEmbed()
-            .setColor("BLURPLE")
-            .setTitle(`Application From ${interaction.user.tag}`)
-            .setDescription(
-              `${interaction.user} <t:${Math.floor(Date.now() / 1000)}:R>`
-            )
-            .addFields([
-              {
-                name: `What is your name?`,
-                value: `> ${user_name}`,
-              },
-              {
-                name: `Why do you want to join staff?`,
-                value: `> ${user_why}`,
-              },
-                {
-                    name: `What previous experience do you carry?`,
-                    value: `> ${user_experience}`,
-                },
-            ])
-            .setFooter({
-              text: `${interaction.user.id}`,
-              iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
-            }),
-        ],
-        components: [btnrow],
-      });
-
-      interaction.reply({
-        content: `Good job! You're application has been submitted for review. Please give us a few days/weeks to review your application.`,
-        ephemeral: true,
-      });
-
-      await interaction.member.roles.add(settings.waitingrole).catch(e => {})
-    }
-  });
+            await interaction.member.roles.add(settings.waitingrole).catch(e => { })
+        }
+    });
 };
--- index.js
+++ index.js
@@ -2,39 +2,39 @@ const { Client } = require("discord.js");
 const settings = require("./config");
 const keep_alive = require('./keep_alive.js')
 const client = new Client({
-  intents: ["GUILDS", "GUILD_MEMBERS", "DIRECT_MESSAGES"],
+    intents: ["GUILDS", "GUILD_MEMBERS", "DIRECT_MESSAGES"],
 });
 
 client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
- // client.user.setActivity(`007_link sucks`);
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+    // client.user.setActivity(`007_link sucks`);
 });
 client.on('ready', () => {
-    setInterval(async ()=>{
-        let textList = ['Fortnite','You','Coding videos', 'Donald Trump', 'The news', `Developers working on Tech Optimum`]
+    setInterval(async () => {
+        let textList = ['Fortnite', 'You', 'Coding videos', 'Donald Trump', 'The news', `Developers working on Tech Optimum`]
         var text = textList[Math.floor(Math.random() * textList.length)];
-        client.user.setActivity(text , { type: 'WATCHING' })
-    },50000) // milliseconds
+        client.user.setActivity(text, { type: 'WATCHING' })
+    }, 50000) // milliseconds
 });
 
 client.on("ready", async () => {
-  console.log(`Online ${client.user.tag}`);
-  let guild = client.guilds.cache.get(settings.guildID);
-  if (guild) {
-    await guild.commands.set([
-      {
-        name: "setup",
-        description: `setup application system in your server`,
-        type: "CHAT_INPUT",
-      },
-      {
-        name: "ping",
-        description: `get ping of bot`,
-        type: "CHAT_INPUT",
-      },
-    ]);
-  }
-  require("./application_manager")(client, settings);
+    console.log(`Online ${client.user.tag}`);
+    let guild = client.guilds.cache.get(settings.guildID);
+    if (guild) {
+        await guild.commands.set([
+            {
+                name: "setup",
+                description: `setup application system in your server`,
+                type: "CHAT_INPUT",
+            },
+            {
+                name: "ping",
+                description: `get ping of bot`,
+                type: "CHAT_INPUT",
+            },
+        ]);
+    }
+    require("./application_manager")(client, settings);
 });
 
 client.login(process.env.TOKEN);
